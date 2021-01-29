740504d6487659d588a73db86838b7df3f0f5a4b
davisantosr <davi-rocha2@hotmail.com>
2021-01-29 13:18:16 -0300

    add info to a blog model to handle at admin area customization

--- blog/admin.py
+++ blog/admin.py
@@ -1,27 +1,47 @@
 from django.contrib import admin
-import django.apps
 
-models = django.apps.apps.get_models()
+from .models import Post, Author
 
-print(models)
+admin.site.register(Post)
+admin.site.register(Author)
 
-for model in models:
-  try:
-    admin.site.register(model)
-  except admin.sites.AlreadyRegistered:
-    pass
 
-admin.site.unregister(django.contrib.contenttypes.models.ContentType)
+
+
+
+
+
+
+
+
+
+
+
 
 # from django.contrib import admin
-# from . import models
+# import django.apps
 
 # models = django.apps.apps.get_models()
 
 # print(models)
 
-# class BlogAdminArea(admin.AdminSite):
-#   site_header = 'Blog Admin Area'
+# for model in models:
+#   try:
+#     admin.site.register(model)
+#   except admin.sites.AlreadyRegistered:
+#     pass
 
-# blog_site = BlogAdminArea(name='BlogAdmin')
+# admin.site.unregister(django.contrib.contenttypes.models.ContentType)
+
+# # from django.contrib import admin
+# # from . import models
+
+# # models = django.apps.apps.get_models()
+
+# # print(models)
+
+# # class BlogAdminArea(admin.AdminSite):
+# #   site_header = 'Blog Admin Area'
+
+# # blog_site = BlogAdminArea(name='BlogAdmin')
 
--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,19 @@
 from django.db import models
 
+
+class Author(models.Model):
+  name = models.CharField(max_length=100)
+  age = models.IntegerField()
+
+  def __str__(self):
+    return self.name
+
 class Post(models.Model):
-  title = models.CharField(max_length=100)
+  author = models.ForeignKey(Author, on_delete=models.CASCADE)
+  title = models.CharField(max_length=40)
+  content = models.TextField(max_length=300)
+  slug = models.SlugField(max_length=200, unique_for_date='publish')
+  postedAt = models.DateField()
 
   def __str__(self):
     return self.title
