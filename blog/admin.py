636c10496c901d8106a63b8bd649c6410ab4597a
davisantosr <davi-rocha2@hotmail.com>
2021-01-29 16:11:25 -0300

    add templates to the admin | overriding templates | need run collectstatic command

--- blog/admin.py
+++ blog/admin.py
@@ -1,59 +1,11 @@
 from django.contrib import admin
-
-from .models import Post
-
-TEXT = 'This is where you put some title and define the author'
+from . import models
 
 
+class BlogAdminArea(admin.AdminSite):
+  site_header = 'Blog Admin Area'
+  login_template = 'blog/admin/login.html'
 
-class PostAdmin(admin.ModelAdmin):
-  fieldsets = (
-    ('Section A', {
-      'fields': ('title', 'author',),
-      'description': '%s' % TEXT,
-    }),
+blog_site = BlogAdminArea(name='BlogAdmin')
 
-    ('Section B', {
-      'fields': ('slug',),
-      'classes': ('collapse',),
-    }),
-  )
-
-
-
-admin.site.register(Post, PostAdmin)
-
-
-
-
-
-
-
-
-# from django.contrib import admin
-# import django.apps
-
-# models = django.apps.apps.get_models()
-
-# print(models)
-
-# for model in models:
-#   try:
-#     admin.site.register(model)
-#   except admin.sites.AlreadyRegistered:
-#     pass
-
-# admin.site.unregister(django.contrib.contenttypes.models.ContentType)
-
-# # from django.contrib import admin
-# # from . import models
-
-# # models = django.apps.apps.get_models()
-
-# # print(models)
-
-# # class BlogAdminArea(admin.AdminSite):
-# #   site_header = 'Blog Admin Area'
-
-# # blog_site = BlogAdminArea(name='BlogAdmin')
-
+blog_site.register(models.Post)
--- core/urls.py
+++ core/urls.py
@@ -1,12 +1,17 @@
 from django.contrib import admin
 from django.urls import path
+from blog.admin import blog_site
+
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
-
     path('admin/', admin.site.urls),
+    path('blogadmin/', blog_site.urls),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-]
+
 
 
 # from django.contrib import admin
