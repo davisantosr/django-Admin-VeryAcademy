a64f6b998e6e9c793c583b0b2ee095288c32d68e
davisantosr <davi-rocha2@hotmail.com>
2021-05-25 11:18:08 -0300

    get all models and registering them

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 from . import models
 
+import django.apps
+
+# models = django.apps.apps.get_models() # Getting all models from the application
+# registering models in a for loop
+# for model in models:
+#   try:
+#     admin.site.register(model)
+#   except admin.sites.AlreadyRegistered:
+#     pass
+
+
 
 class BlogAdminArea(admin.AdminSite):
   site_header = 'Blog Admin Area'
