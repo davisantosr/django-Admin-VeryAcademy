7f7c801b94a3f85ceacf80ec0f02d222787b7ee8
davisantosr <davi-rocha2@hotmail.com>
2021-05-25 13:11:53 -0300

    add group verification

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
 from . import models
 
 class BlogAdminArea(admin.AdminSite):
@@ -11,7 +12,9 @@ class TestAdminPermissions(admin.ModelAdmin):
     return True
 
   def has_change_permission(self, request, obj=None) -> bool:
-      return True
+      # if request.user.groups.filter(name='nameofsomegroup').exists():
+      #   return True
+    return True
 
   def has_view_permission(self, request, obj=None):
     return True
@@ -25,3 +28,4 @@ class TestAdminPermissions(admin.ModelAdmin):
 blog_site = BlogAdminArea(name='BlogAdmin')
 
 blog_site.register(models.Post, TestAdminPermissions)
+blog_site.register(User)
