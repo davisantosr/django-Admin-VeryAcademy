fa61c14efb04678186e71f951330db7753c21830
davisantosr <davi-rocha2@hotmail.com>
2021-06-01 20:33:28 -0300

    csv upload from admin

--- blog/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-from django.contrib.admin.apps import AdminConfig
-
-class BlogAdminConfig(AdminConfig):
-    default_site = 'blog.admin.BlogAdminArea'
-
-class BlogConfig(AppConfig):
-    name = 'blog'
